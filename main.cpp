4bda0ed49e8a067d277722ba92f44e03fc9bb9b6
Vsevolod Bolotov <VYBolotov24@codingburgas.bg>
2026-05-09 00:17:51 +0300

    feat: add logo to the app

--- main.cpp
+++ main.cpp
@@ -6,6 +6,8 @@
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
 
+    app.setWindowIcon(QIcon(":/logo/Certa.dev_clean.png"));
+
     QPixmapCache::setCacheLimit(65536);
 
     if (!Database::instance().connect("Certadev.db")) {
